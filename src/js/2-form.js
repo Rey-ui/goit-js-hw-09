f7e2e02f3e76d71a07827dfeec6fc5444fc02c99
trubchaninov Andriy 1-8 <a.qwerty0444@gmail.com>
2024-01-03 15:51:59 +0200

    add form

---
+++ src/js/2-form.js
@@ -0,0 +1,29 @@
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+const formEl = document.querySelector(".feedback-form");
+const inputEl = formEl.querySelector('input[name="email"]');
+const textareaEl = formEl.querySelector('textarea[name="message"]');
+let formData = {};
+formEl.addEventListener("input", writeMasseg);
+formEl.addEventListener("submit", submitForm);
+function submitForm(event) {
+    event.preventDefault();
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+    if (formData.email !== "" && formData.message !== "") {
+        console.log(formData);
+    }
+    event.currentTarget.reset();
+}
+window.addEventListener("load", () => {
+    const saveLocalSotr = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+    inputEl.value = saveLocalSotr.email || "";
+    textareaEl.value = saveLocalSotr.message || "";
+})
+function writeMasseg() {
+    formData = {
+        email: inputEl.value.trim(),
+        message: textareaEl.value.trim()
+    }
+    if ((formData.email.length > 0) || (formData.message.length > 0)) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+    }
+}
