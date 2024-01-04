d1f326cce6f37aebe652ccd996de3908ff7b701e
trubchaninov Andriy 1-8 <a.qwerty0444@gmail.com>
2024-01-04 12:45:18 +0200

    update 1-gallary.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,4 +1,5 @@
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const images = [
     {
         preview:
@@ -79,6 +80,7 @@ const elemsLi = images.map(image => {
 
 galleryEl.insertAdjacentHTML("beforeend", elemsLi);
 const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
     captionPosition: 'bottom',
     captionDelay: 250,
 });
